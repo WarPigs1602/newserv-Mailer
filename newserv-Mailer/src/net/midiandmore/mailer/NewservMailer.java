436fa2b7ec754330806d2052d405e8a1736a2d56
WarPigs1602 <gh@midiandmore.de>
2024-08-16 10:37:43 +0200

    Fixed encryption for urls

--- newserv-Mailer/src/net/midiandmore/mailer/NewservMailer.java
+++ newserv-Mailer/src/net/midiandmore/mailer/NewservMailer.java
@@ -21,6 +21,7 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import org.apache.commons.codec.binary.Hex;
 import static org.apache.commons.codec.digest.DigestUtils.sha256Hex;
 import static org.apache.commons.codec.digest.HmacAlgorithms.HMAC_MD5;
@@ -76,10 +77,20 @@ public class NewservMailer {
 
     private String generateUrl(String[] obj) {
         var r = String.valueOf(new Random(4).nextInt()).getBytes();
-        var uid = Integer.parseInt(obj[0]);
         var uname = obj[1];
         var password = obj[11];
         var key = getConfig().getConfigFile().getProperty("urlkey");
+        var a = MD5("%s %s".formatted(r, key));
+        try {
+            a = Hex.encodeHexString(RC4(a, password));
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException ex) {
+            Logger.getLogger(NewservMailer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        var b = MD5(MD5("%s %s %s %s".formatted(getConfig().getConfigFile().getProperty("urlsecret"), uname, a, r)));
+        return "%s?m=%s&h=%s&u=%s&r=%s".formatted(getConfig().getConfigFile().getProperty("url"), a, b, Hex.encodeHexString(uname.getBytes()), Hex.encodeHexString(r));
+    }
+
+    private String MD5(String text) {
         MessageDigest messageDigest = null;
         try {
             messageDigest = MessageDigest.getInstance("MD5");
@@ -87,19 +98,9 @@ public class NewservMailer {
             Logger.getLogger(NewservMailer.class.getName()).log(Level.SEVERE, null, ex);
         }
         messageDigest.reset();
-        messageDigest.update("%s %s".formatted(r, key).getBytes(Charset.forName("UTF8")));
+        messageDigest.update(text.getBytes(Charset.forName("UTF8")));
         var resultByte = messageDigest.digest();
-        var a = new String(Hex.encodeHex(resultByte));
-        try {
-            a = new Hex(RC4(a, password)).toString();
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException ex) {
-            Logger.getLogger(NewservMailer.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        messageDigest.reset();
-        messageDigest.update("%s %s %s %s".formatted(getConfig().getConfigFile().getProperty("urlsecret"), uname, a, r).getBytes(Charset.forName("UTF8")));
-        resultByte = messageDigest.digest();
-        var b = new String(Hex.encodeHex(resultByte));
-        return "%s?m=%s&h=%s&u=%s&r=%s".formatted(getConfig().getConfigFile().getProperty("url"), a, b, uname, new String(Hex.encodeHex(r)));
+        return Hex.encodeHexString(resultByte);
     }
 
     private String generateActivationUrl(String[] obj) {
@@ -110,8 +111,9 @@ public class NewservMailer {
         var key = getConfig().getConfigFile().getProperty("activationkey");
         String a = null;
         var hex = sha256Hex("%s %s %s".formatted(r, key, password));
+        var rc4 = Hex.encodeHexString(password.getBytes());
         try {
-            a = new String(Hex.encodeHexString(RC4(hex, new String(Hex.encodeHexString(password.getBytes()))).getBytes()));
+            a = Hex.encodeHexString(RC4(hex, rc4));
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException ex) {
             Logger.getLogger(NewservMailer.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -120,16 +122,14 @@ public class NewservMailer {
         return "%s?id=%d&h=%s&r=%s&u=%s&p=%s".formatted(getConfig().getConfigFile().getProperty("activationurl"), uid, hd, r, Hex.encodeHexString(uname.getBytes()), a);
     }
 
-    private String RC4(String text, String part2) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
-        byte[] testDataBytes = text.getBytes();
-        KeyGenerator rc4KeyGenerator = KeyGenerator.getInstance("RC4");
-        SecretKey key = rc4KeyGenerator.generateKey();
+    private byte[] RC4(String text, String part2) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        byte[] part = text.getBytes();
+        SecretKey key = new SecretKeySpec(part, "RC4");
         // Create Cipher instance and initialize it to encrytion mode
         Cipher cipher = Cipher.getInstance("RC4");  // Transformation of the algorithm
         cipher.init(Cipher.ENCRYPT_MODE, key);
-        cipher.update(part2.getBytes());
-        byte[] cipherBytes = cipher.doFinal(testDataBytes);
-        return new String(cipherBytes);
+        byte[] cipherBytes = cipher.doFinal(part2.getBytes());
+        return cipherBytes;
     }
 
     private void email(String userid, String emailtype, String prevemail) {
@@ -144,8 +144,8 @@ public class NewservMailer {
         }
         var email = one[12];
         var template = (String[]) getConfig().getTemplateFile().get(emailtype);
-        var msg = parseTemplate(template[1], one, prevemail);
-        var subject = parseTemplate(template[0], one, prevemail);
+        var msg = parseTemplate(template[1], one, prevemail, Integer.parseInt(emailtype));
+        var subject = parseTemplate(template[0], one, prevemail, Integer.parseInt(emailtype));
         try {
             getMail().sendEmail(msg, subject, email);
             System.out.printf("Email with subject: %s, sended to : %s\n", subject, email);
@@ -170,9 +170,21 @@ public class NewservMailer {
         return arr;
     }
 
-    private String parseTemplate(String content, String[] one, String prevemail) {
-        var code = generateResetcode(one);
-        var url = generateActivationUrl(one);
+    private String parseTemplate(String content, String[] one, String prevemail, int mailtype) {
+        String[] code = {"", "", ""};
+        var url = "";
+        if (mailtype == 1) {
+            url = generateUrl(one);
+        }
+        if (mailtype == 3) {
+            code = generateResetcode(one);
+        }
+        if (mailtype == 5) {
+            code = generateResetcode(one);
+        }
+        if (mailtype == 6) {
+            url = generateActivationUrl(one);
+        }
         content = content.replace("%(url)s", url);
         content = content.replace("%(resetline)s", code[2]);
         content = content.replace("%(resetcode)s", code[1]);
